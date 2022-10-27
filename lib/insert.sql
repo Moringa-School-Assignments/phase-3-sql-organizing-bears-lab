3a65fe6da99c395a06c8988add0d1655761a97e3
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-27 22:10:44 +0300

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr Chocolate', 36, 'M', 'brown', 'awesome', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 21, 'M', 'white', 'cool', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 46, 'F', 'white', 'sad', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 24, 'F', 'indian', 'brave', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 87, 'F', 'asian', 'happy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 36, 'M', 'brown', 'awesome', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 36, 'M', 'brown', 'awesome', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NUll, 36, 'M', 'black', 'mysterious', 1);
